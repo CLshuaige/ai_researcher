216d659cc9132f473d776c0bf9a7baf18713f87d
chenl <12415038@zju.edu.cn>
2026-01-16 10:56:58 +0000

    更新serve.sh

--- local_serve.sh
+++ local_serve.sh
@@ -1,11 +1,13 @@
 
 vllm serve \
   /home/ai_researcher/.cache/modelscope/hub/models/Qwen/Qwen3-30B-A3B-Instruct-2507 \
-  --tensor-parallel-size 4 \
+  --tensor-parallel-size 8 \
   --gpu-memory-utilization 0.90 \
   --max-model-len 32768 \
   --dtype bfloat16 \
   --trust-remote-code \
   --host 0.0.0.0 \
-  --port 8001
+  --port 8001 \
+  --enable-auto-tool-choice \
+  --tool-call-parser pythonic
 
